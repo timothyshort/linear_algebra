e687798b296bf8c20d929f7c0e0270949f8286eb
Tim <timothyjames.short@gmail.com>
2017-08-25 10:40:41 -0400

    Added outline for rref solution

--- python_scripts/linsys.py
+++ python_scripts/linsys.py
@@ -72,7 +72,7 @@ class LinearSystem(object):
 
             #Make sure we find our pivot row by having a leading non-zero coefficient
             if system[row].normal_vector[row] == 0:
-                print "LEADING TERM = 0 | SWAP WITH FIRST NON-ZERO COEFFICIENT"
+                print "LEADING TERM = 0"
                 #Find the first leading non-zero term
                 for rowLeadingNonZero in range(row+1,len(system)-1):
                     if system[rowLeadingNonZero] != 0 :
@@ -94,7 +94,16 @@ class LinearSystem(object):
             
             print ""; print system
             print "*******\nIteration"
-        return system        
+        return system
+
+    def compute_rref(self):
+        tf = self.compute_triangular_form()
+
+        #Find values of variables
+        #Plug them in above
+        #Divide each pivot row by leading coefficient (1/leadingCoefficient)
+
+        return tf       
 
 
     def indices_of_first_nonzero_terms_in_each_row(self):
@@ -253,6 +262,50 @@ def rowOpsTest():
 
     print s
 
+def rrefTest():
+    print "******************\n   TEST CASE 1\n******************"
+    p1 = Plane(normal_vector=Vector([1,1,1]), constant_term=1)
+    p2 = Plane(normal_vector=Vector([0,1,1]), constant_term=2)
+    s = LinearSystem([p1,p2])
+    r = s.compute_rref()
+    if not (r[0] == Plane(normal_vector=Vector([1,0,0]), constant_term=-1) and
+            r[1] == p2):
+        print 'test case 1 failed'
+
+    print "******************\n   TEST CASE 2\n******************"
+    p1 = Plane(normal_vector=Vector([1,1,1]), constant_term=1)
+    p2 = Plane(normal_vector=Vector([1,1,1]), constant_term=2)
+    s = LinearSystem([p1,p2])
+    r = s.compute_rref()
+    if not (r[0] == p1 and
+            r[1] == Plane(constant_term=1)):
+        print 'test case 2 failed'
+
+    print "******************\n   TEST CASE 3\n******************"
+    p1 = Plane(normal_vector=Vector([1,1,1]), constant_term=1)
+    p2 = Plane(normal_vector=Vector([0,1,0]), constant_term=2)
+    p3 = Plane(normal_vector=Vector([1,1,-1]), constant_term=3)
+    p4 = Plane(normal_vector=Vector([1,0,-2]), constant_term=2)
+    s = LinearSystem([p1,p2,p3,p4])
+    r = s.compute_rref()
+    if not (r[0] == Plane(normal_vector=Vector([1,0,0]), constant_term=0) and
+            r[1] == p2 and
+            r[2] == Plane(normal_vector=Vector([0,0,-2]), constant_term=2) and
+            r[3] == Plane()):
+        print 'test case 3 failed'
+
+    print "******************\n   TEST CASE 4\n******************"
+    p1 = Plane(normal_vector=Vector([0,1,1]), constant_term=1)
+    p2 = Plane(normal_vector=Vector([1,-1,1]), constant_term=2)
+    p3 = Plane(normal_vector=Vector([1,2,-5]), constant_term=3)
+    s = LinearSystem([p1,p2,p3])
+    r = s.compute_rref()
+    if not (r[0] == Plane(normal_vector=Vector([1,0,0]), constant_term=Decimal(23)/Decimal(9)) and
+            r[1] == Plane(normal_vector=Vector([0,1,0]), constant_term=Decimal(7)/Decimal(9)) and
+            r[2] == Plane(normal_vector=Vector([0,0,1]), constant_term=Decimal(2)/Decimal(9))):
+        print 'test case 4 failed'
+
 
 #rowOpsTest()
-triangularFormTest()
+#triangularFormTest()
+rrefTest()
